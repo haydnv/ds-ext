b42e6dd429974b950ed64e7291a1c09c42e80400
Haydn Vestal <haydnv@gmail.com>
2023-02-06 15:50:21 +0530

    delete bree and hash modules

--- src/btree/collection.rs
+++
@@ -1,8 +0,0 @@
-//! A [`Collection`] with no trait boundaries, i.e. a [`BTreeMap`] which generates its own keys.
-
-use super::map::BTreeMap;
-
-/// A generic collection with no trait boundaries, i.e. a [`BTreeMap`] which generates its own keys.
-pub struct Collection<T> {
-    inner: BTreeMap<usize, T>,
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! This crate repackages data structures from the Rust standard library
-//! with additional capabilities like ordered maps and sets, segmented URL-safe paths,
+//! with additional capabilities like fast ordered maps and sets, segmented URL-safe paths,
 //! and `Collection` types with no trait boundaries.
 //!
 //! The ordered collection types use a [`List`] internally for ordering.
@@ -7,12 +7,8 @@
 //! The map and set types support a `Key` trait to allow using arbitrary type `T: Key<K>`
 //! to look up an entry with key type `K`.
 
-pub mod btree;
-pub mod hash;
 pub mod ord;
 pub mod path;
 
-pub use btree::{BTreeMap, BTreeSet};
-pub use hash::{HashMap, HashSet};
-pub use ord::List;
+pub use ord::{LinkedHashMap, List};
 pub use path::{Host, Id, Link, Path, PathBuf, PathSegment};
