b5ad17d68895087ad672e82fda5fd6af6c4fb492
Haydn Vestal <haydnv@gmail.com>
2024-08-29 10:25:43 +0800

    add usage examples

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ds-ext
+
+[![Crates.io][crates-badge]][crates-url]
+[![Docs.rs][docs-badge]][docs-url]
+
+[crates-badge]: https://img.shields.io/crates/v/ds-ext.svg
+[crates-url]: https://crates.io/crates/ds-ext
+[docs-badge]: https://docs.rs/ds-ext/badge.svg
+[docs-url]: https://docs.rs/ds-ext/
+
 Extensions to standard Rust data structures which provide additional capabilities
+
+Example usage:
+```rust
+use ds_ext::*;
+
+let mut set = OrdHashSet::new();
+set.insert(1);
+assert!(set.contains(&1));
+
+let mut map = OrdHashMap::from_iter(set.into_iter().map(|i| (i, i)));
+assert_eq!(map.get(&1), map.bisect(|i| i.partial_cmp(&1)));
+//! ```
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,24 @@
 //!  - `serialize`: enables support for [`serde`](https://docs.rs/serde/).
 //!  - `stream`: enables support for [`destream`](https://docs.rs/destream/).
 //!  - `hash`: enables support for [`async-hash`](https://docs.rs/async-hash/).
+//!
+//!
+//! Example usage:
+//! ```
+//! use ds_ext::*;
+//!
+//! let mut set = OrdHashSet::new();
+//! assert!(set.is_empty());
+//!
+//! set.insert(1);
+//! assert!(set.contains(&1));
+//! assert_eq!(set.len(), 1);
+//!
+//! let mut map = OrdHashMap::from_iter(set.into_iter().map(|i| (i, i)));
+//! assert_eq!(map.len(), 1);
+//!
+//! assert_eq!(map.get(&1), map.bisect(|i| i.partial_cmp(&1)));
+//! ```
 
 #[cfg(feature = "hash")]
 mod hash;
